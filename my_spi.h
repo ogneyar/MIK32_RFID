37acca8e0dec2606d8bef9f418ed8bc2eaebd324
Ogneyar <ogneyar@hutoryanin.ru>
2024-08-02 19:50:41 +0300

    пока у Микрона либа кривая (https://github.com/MikronMIK32/mik32-hal) - сделал такой костыль (:

--- my_spi.h
+++ my_spi.h
@@ -69,7 +69,7 @@ void SPI_SendData(uint8_t Data)
 void SPI_SendArray(uint8_t master_output[], uint8_t length)
 {
     uint8_t master_input[sizeof(master_output)];
-    HAL_StatusTypeDef SPI_Status = HAL_SPI_Exchange(&hspi1, master_output, master_input, length, SPI_TIMEOUT_DEFAULT);
+    HAL_StatusTypeDef SPI_Status = SPI_Exchange(&hspi1, master_output, master_input, length, SPI_TIMEOUT_DEFAULT);
     if (SPI_Status != HAL_OK)
     {
         xprintf("SPI_Error %d, OVR %d, MODF %d\r\n", SPI_Status, hspi1.ErrorCode & HAL_SPI_ERROR_OVR, hspi1.ErrorCode & HAL_SPI_ERROR_MODF);
@@ -89,7 +89,7 @@ uint8_t SPI_transfer(uint8_t data)
     uint8_t master_output[] = { data };
     uint8_t master_input[sizeof(master_output)];
 
-    HAL_StatusTypeDef SPI_Status = HAL_SPI_Exchange(&hspi1, master_output, master_input, sizeof(master_output), SPI_TIMEOUT_DEFAULT);
+    HAL_StatusTypeDef SPI_Status = SPI_Exchange(&hspi1, master_output, master_input, sizeof(master_output), SPI_TIMEOUT_DEFAULT);
     if (SPI_Status != HAL_OK)
     {
         xprintf("SPI_Error %d, OVR %d, MODF %d\r\n", SPI_Status, hspi1.ErrorCode & HAL_SPI_ERROR_OVR, hspi1.ErrorCode & HAL_SPI_ERROR_MODF);
@@ -97,6 +97,68 @@ uint8_t SPI_transfer(uint8_t data)
     }
 
  	return master_input[0];
+}
+
+// пока у Микрона либа кривая (https://github.com/MikronMIK32/mik32-hal) - сделал такой костыль (:
+HAL_StatusTypeDef SPI_Exchange(SPI_HandleTypeDef *hspi, uint8_t TransmitBytes[], uint8_t ReceiveBytes[], uint32_t DataSize, uint32_t Timeout)
+{
+    uint32_t txallowed = 1;
+    HAL_StatusTypeDef error_code = HAL_OK;
+    uint32_t timeout_counter = 0;
+
+    hspi->ErrorCode = HAL_SPI_ERROR_NONE;
+    hspi->pRxBuffPtr = (uint8_t *)ReceiveBytes;
+    hspi->RxCount = DataSize;
+    hspi->pTxBuffPtr = (uint8_t *)TransmitBytes;
+    hspi->TxCount = DataSize;
+
+    hspi->Instance->TX_THR = 1;
+
+    /* Включить SPI если выключено */
+    if (!(hspi->Instance->ENABLE & SPI_ENABLE_M))
+    {
+        __HAL_SPI_ENABLE(hspi);
+    }
+
+    while ((hspi->TxCount > 0) || (hspi->RxCount > 0))
+    {
+        /* Проверка флага TX_FIFO_NOT_FULL */
+        if ((hspi->Instance->INT_STATUS & SPI_INT_STATUS_TX_FIFO_NOT_FULL_M) && (hspi->TxCount > 0) && (txallowed == 1))
+        {
+            hspi->Instance->TXDATA = *(hspi->pTxBuffPtr);
+            hspi->pTxBuffPtr++;
+            hspi->TxCount--;
+            /* Следующие данные - прием (Rx). Tx не разрешен */
+            txallowed = 0;
+        }
+
+        /* Ожидание когда установится флаг RX_FIFO_NOT_EMPTY */
+        if ((hspi->Instance->INT_STATUS & SPI_INT_STATUS_RX_FIFO_NOT_EMPTY_M) && (hspi->RxCount > 0))
+        {
+            *(hspi->pRxBuffPtr) = hspi->Instance->RXDATA;
+            hspi->pRxBuffPtr++;
+            hspi->RxCount--;
+            /* Следующие данные - передача (Tx). Tx разрешается */
+            txallowed = 1;
+        }
+
+        if (((timeout_counter++) >= Timeout) || (Timeout == 0U))
+        {
+            error_code = HAL_TIMEOUT;
+            goto error;
+        }
+    }
+    
+    return error_code; // добавил одну строчку и теперь у меня SPI работает
+
+error:
+    __HAL_SPI_DISABLE(hspi);
+    hspi->Instance->ENABLE |= SPI_ENABLE_CLEAR_TX_FIFO_M | SPI_ENABLE_CLEAR_RX_FIFO_M; /* Очистка буферов RX и TX */
+    volatile uint32_t unused = hspi->Instance->INT_STATUS; /* Очистка флагов ошибок чтением */
+    (void) unused;
+
+
+    return error_code;
 }
 
 
